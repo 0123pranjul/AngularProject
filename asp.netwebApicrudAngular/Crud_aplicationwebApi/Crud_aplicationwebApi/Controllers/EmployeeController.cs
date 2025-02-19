9fb73d672596f8cf4deb8fb8e77a99af6766e77e
PRANJUL SACHAN <0123pranjul@gmail.com>
2025-02-19 18:50:12 +0530

    Inisial Commite

---
+++ asp.netwebApicrudAngular/Crud_aplicationwebApi/Crud_aplicationwebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using Crud_aplicationwebApi.Models;
+using Crud_aplicationwebApi.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud_aplicationwebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmpLoyeeRepository emp;
+
+        public EmployeeController(EmpLoyeeRepository empLoyeeRepository)
+        {
+            this.emp = empLoyeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EmployeeList()
+        {
+            var allemp = await emp.GetAllEmployees();
+            return Ok(allemp);
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddEmployee( Employee vm)
+        {
+            await emp.SaveEmployee(vm);
+            return Ok(vm);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateEmployee(int id, [FromBody] Employee vm)
+        {
+            await emp.updateEmployee(id, vm);
+            return Ok(vm);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult>deleteEmployee(int id)
+        {
+            await emp.DeleteEmployee(id);
+            return Ok();
+        }
+    }
+}
---
+++ asp.netwebApicrudAngular/Crud_aplicationwebApi/Crud_aplicationwebApi/Repository/EmpLoyeeRepository.cs
@@ -0,0 +1,53 @@
+﻿using Crud_aplicationwebApi.Data;
+using Crud_aplicationwebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_aplicationwebApi.Repository
+{
+    public class EmpLoyeeRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public EmpLoyeeRepository( AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            return await dbContext.Employees.ToListAsync();
+        }
+        public async Task SaveEmployee(Employee emp)
+        {
+            await dbContext.Employees.AddAsync(emp);
+            await dbContext.SaveChangesAsync();
+        }
+      
+        public async Task updateEmployee(int id, Employee obj)
+        {
+            var employee = await dbContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                throw new Exception("Employee Not Found");
+            }
+            employee.Name = obj.Name;
+            employee.Email = obj.Email;
+            employee.Mobile = obj.Mobile;
+            employee.Age = obj.Age;
+            employee.Salary = obj.Salary;
+            employee.Status = obj.Status;
+
+            await dbContext.SaveChangesAsync();
+        }
+        public async Task DeleteEmployee(int id)
+        {
+            var employees = await dbContext.Employees.FindAsync(id);
+            if(employees==null)
+            {
+                throw new Exception("Employee Not Found");
+            }
+            dbContext.Employees.Remove(employees);
+            await dbContext.SaveChangesAsync();
+        }
+
+    }
+}
